d51383951467019c81f30736f636103c26e7b7eb
jmskeoch <jmskeoch@yahoo.com>
2020-09-25 02:41:41 +0000

    Add rollDie() code + sumOfTwoDice() code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,20 +5,34 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        return (int) ((Math.random() * 6) + 1);
     }
 
     // Rolls two dice n times
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
-        return null;
+        int[] sumList = new int[11];
+        int sum;
+        
+        for(int i = 0; i < n; i++) {
+            sum = rollDie() + rollDie();
+            sumList[sum-2]++;
+        }
+        return sumList;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("How many pairs of dice would you like to roll?: ");
+        int n = scan.nextInt();
+        int[] sums = sumOfTwoDice(n);
+
+        for(int i = 0; i < 11; i++) {
+            System.out.println("% of rolls where sum == " + (i + 2) + ": " + ((double) sums[i] / n * 100));
+        }
+        scan.close();
     }
 }
